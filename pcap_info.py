a9119073ff329970a3f50ff22fd2a869ff14729c
Kyle Isom <coder@kyleisom.net>
2012-07-30 15:27:18 -0600

    pcap_info.py uses built-in repr now

--- pcap_info.py
+++ pcap_info.py
@@ -8,11 +8,7 @@ def pcap_info(pcap_file):
     sf = savefile.load_savefile(pcap_file)
 
     if sf.valid:
-        print 'Packet capture file with format %u.%u:' % (sf.header.major,
-                                                          sf.header.minor)
-        print '\tbyte order: %s endian' % (sf.byteorder,)
-        print '\tsnapshot length: %u' % (sf.header.snaplen,)
-        print '\tlink layer type: %s' % (linklayer.lookup(sf.header.ll_type), )
+        print sf
     else:
         print 'Invalid packet capture!'
 
