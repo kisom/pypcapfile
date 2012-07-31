eef50f5fef289a66b53a16d3353409b8be2e3478
Kyle Isom <coder@kyleisom.net>
2012-07-30 18:52:30 -0600

    code cleanup

--- pcapfile/linklayer.py
+++ pcapfile/linklayer.py
@@ -1,16 +1,48 @@
-__LL_TYPES__ = [('LINKTYPE_NULL', 0),
-                ('LINKTYPE_ETHERNET', 1),
-                ('LINKTYPE_TOKEN_RING', 6),
-                ('LINKTYPE_ARCNET', 7),
-                ('LINKTYPE_SLIP', 8)]
+# contains definitions of link layer types from libpcap
+"""
+Link layer definitions and lookup functions based on libpcap's LLTYPE_*
+values.
+"""
 
-def lookup(ll_type):
 
+__LL_TYPES__ = [('LINKTYPE_NULL', 0, 'null'),
+                ('LINKTYPE_ETHERNET', 1, 'ethernet'),
+                ('LINKTYPE_TOKEN_RING', 6, 'token ring'),
+                ('LINKTYPE_ARCNET', 7, 'ARCnet'),
+                ('LINKTYPE_SLIP', 8, 'SLIP')]
+
+
+def __get_ll_type__(ll_type):
+    """
+    Given an lltype value, retrieve its definition.
+    """
     res = [llt for llt in __LL_TYPES__
            if llt[1] == ll_type]
     assert len(res) < 2, 'Duplicate linklayer types.'
 
     if res:
-        return res[0][0]
+        return res[0]
     else:
         return None
+
+
+def lookup(ll_type):
+    """
+    Given an ll_type, retrieve the appropriate LL_TYPE.
+    """
+    res = __get_ll_type__(ll_type)
+    if res:
+        return res[0]
+    else:
+        return res
+
+
+def slookup(ll_type):
+    """
+    Given an ll_type, retrieve the short name for the link layer.
+    """
+    res = __get_ll_type__(ll_type)
+    if res:
+        return res[2]
+    else:
+        return res
