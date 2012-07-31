690b50d1aaa1fc6c2468a6cb9c4933a75c8e2733
Kyle Isom <coder@kyleisom.net>
2012-07-31 01:28:33 -0600

    code clean up and add strip function
    add new function strip_ip() to remove the IP header from the packet.

--- pcapfile/protocols/ip.py
+++ pcapfile/protocols/ip.py
@@ -6,12 +6,12 @@ import binascii
 import ctypes
 import struct
 
+
 class IP(ctypes.Structure):
     """
     Represents an IP packet.
     """
 
-    packfmt = '!BBHHHBBHII'  # 20 bytes
     _fields_ = [('v', ctypes.c_ushort),         # version
                 ('hl', ctypes.c_ushort),        # internet header length
                 ('tos', ctypes.c_ubyte),        # type of service
@@ -31,10 +31,10 @@ class IP(ctypes.Structure):
     def __init__(self, packet):
         # parse the required header first, deal with options later
         magic = struct.unpack('B', packet[0])[0]
-        assert ((magic & 0b1100) == 4 
+        assert ((magic & 0b1100) == 4
                 and (magic & 0b0111) > 4), 'not an IPv4 packet.'
 
-        fields = struct.unpack(IP.packfmt, packet[:20])
+        fields = struct.unpack('!BBHHHBBHII', packet[:20])
         self.v = fields[0] & 0b1100
         self.hl = fields[0] & 0b0111
         self.tos = fields[1]
@@ -45,27 +45,40 @@ class IP(ctypes.Structure):
         self.ttl = fields[5]
         self.p = fields[6]
         self.sum = fields[7]
-        self.src = ctypes.c_char_p(self.__parse_ipv4(fields[8]))
-        self.dst = ctypes.c_char_p(self.__parse_ipv4(fields[9]))
-    
-        if self.len > 0x14:
+        self.src = ctypes.c_char_p(parse_ipv4(fields[8]))
+        self.dst = ctypes.c_char_p(parse_ipv4(fields[9]))
+
+        if self.hl > 0x14:
             start = 0x15
             end = self.len - 0x14
             self.opt = binascii.hexlify(packet[start:end])
         else:
             self.opt = '\x00'
-            self.payload = binascii.hexlify(packet[0x15:])
-    
+            self.payload = ctypes.c_char_p(binascii.hexlify(packet[0x14:]))
+
         self.pad = '\x00'
 
-    def __parse_ipv4(self, address):
-        raw = struct.pack('I', address)
-        octets = struct.unpack('BBBB', raw)[::-1]
-        ipv4 = '.'.join(['%d' % (b,) for b in octets])
-        return ipv4
 
-    def src_ip(self):
-        return self.__parse_ipv4(self.src)
+def parse_ipv4(address):
+    """
+    Given a raw IPv4 address (i.e. as an unsigned integer), return it in
+    dotted quad notation.
+    """
+    raw = struct.pack('I', address)
+    octets = struct.unpack('BBBB', raw)[::-1]
+    ipv4 = '.'.join(['%d' % (b,) for b in octets])
+    return ipv4
 
-    def dst_ip(self):
-        return self.__parse_ipv4(self.dst)
+
+def strip_ip(packet):
+    """
+    Remove the IP packet layer, yielding the transport layer.
+    """
+    if not type(packet) == IP:
+        packet = IP(packet)
+    payload = packet.payload
+
+    if type(payload) == '':
+        payload = binascii.unhexlify(payload)
+    return payload
+
