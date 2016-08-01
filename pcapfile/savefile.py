07022ff98fe44fce6766cdf95223b79152efc342
Marco 'don' Kaulea <DonMarco42@gmail.com>
2016-08-01 20:57:03 +0200

    Create specific Exceptions for the module
    This allows the user of the library to specificly catch all Exceptions
    from this module by catching the base error definied in the module.

--- pcapfile/savefile.py
+++ pcapfile/savefile.py
@@ -11,8 +11,8 @@ import struct
 import sys
 
 import pcapfile.linklayer as linklayer
-
 from pcapfile.structs import __pcap_header__, pcap_packet
+from pcapfile import InvalidEncoding, UnknownMagicNumber, InvalidHeader
 
 VERBOSE = False
 
@@ -89,7 +89,7 @@ def _load_savefile_header(file_h):
         raw_savefile_header = file_h.read(24)
     except UnicodeDecodeError:
         print("\nMake sure the input file is opened in read binary, 'rb'\n")
-        raise
+        raise InvalidEncoding("Could not read file. Might not be opened as binary.")
 
     # in case the capture file is not the same endianness as ours, we have to
     # use the correct byte order for the file header
@@ -102,14 +102,14 @@ def _load_savefile_header(file_h):
         byte_order = b'little'
         unpacked = struct.unpack('<IhhIIII', raw_savefile_header)
     else:
-        raise Exception('Invalid pcap file.')
+        raise UnknownMagicNumber("No supported Magic Number found")
 
     (magic, major, minor, tz_off, ts_acc, snaplen, ll_type) = unpacked
     header = __pcap_header__(magic, major, minor, tz_off, ts_acc, snaplen,
                              ll_type, ctypes.c_char_p(byte_order),
                              magic == _MAGIC_NUMBER_NS)
     if not __validate_header__(header):
-        raise Exception('invalid savefile header!')
+        raise InvalidHeader("Invalid Header")
     else:
         return header
 
