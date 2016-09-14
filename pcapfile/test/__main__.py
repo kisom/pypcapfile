46adf6948a5a81266d1dcc95646c049d1684b247
Kivanc Cakmak <kivanccakmak@gmail.com>
2016-09-14 18:38:53 -0400

    feat: initial commit on wi-fi unit test
    parsing check added for known packets below.
    
    - RTS(Request to Send)
    - CTS(Clear to Send)
    - Data with A-MSDU (Aggregated Mac Service Data Unit)
    - Data with non A-MSDU
    - Block Acknowledgement

--- pcapfile/test/__main__.py
+++ pcapfile/test/__main__.py
@@ -9,10 +9,12 @@ import unittest
 from pcapfile.test.linklayer_test import TestCase as LinklayerTest
 from pcapfile.test.savefile_test import TestCase as SavefileTest
 from pcapfile.test.protocols_linklayer_ethernet import TestCase as EthernetTest
+from pcapfile.test.protocols_linklayer_wifi import TestCase as WifiTest
 from pcapfile.test.protocols_transport_tcp import TestCase as TcpTest
 
 if __name__ == '__main__':
-    TEST_CLASSES = [SavefileTest, LinklayerTest, EthernetTest, TcpTest]
+    TEST_CLASSES = [SavefileTest, LinklayerTest, EthernetTest, WifiTest,
+            TcpTest]
     SUITE = unittest.TestSuite()
     LOADER = unittest.TestLoader()
     for test_class in TEST_CLASSES:
