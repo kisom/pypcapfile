99010066dc06e279d0c0e10aa9b7e1773ce556a7
johnthagen <johnthagen@users.noreply.github.com>
2017-02-14 19:54:15 -0500

    Add supported Python versions to PyPI page

--- setup.py
+++ setup.py
@@ -27,5 +27,14 @@ setup(name='pypcapfile',
       package_data={'pcapfile.test': ['test/test_data']},
       data_files=[('share/doc/pcapfile',
                    ['README.rst', 'AUTHORS', 'CONTRIBUTING'])]
+      classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        ],
       )
 
